93de7fbddeb87ffce4dcce88a78dab4bd2accd3c
Dima Kirchanov <kirchanov2951@mail.ru>
2025-07-31 04:16:49 +0500

    s

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/vite.maket/',
   css: {
     preprocessorOptions: {
       scss: {
