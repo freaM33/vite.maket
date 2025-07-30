87b3e36b97a9b9463b362f5947ce7c0da3d5fa21
Dima Kirchanov <kirchanov2951@mail.ru>
2025-07-31 03:45:50 +0500

    Исправлена конфигурация Vite для корректной загрузки изображений

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/vite.maket/',
+  base: '/',
   css: {
     preprocessorOptions: {
       scss: {
