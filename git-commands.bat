7274891ed3572b9199e8bc87c78abdaa2ba1830f
Dima Kirchanov <kirchanov2951@mail.ru>
2025-07-31 02:47:39 +0500

    Initial commit: Vite 3 service center project

---
+++ git-commands.bat
@@ -0,0 +1,7 @@
+@echo off
+git add .
+git commit -m "Initial commit: Vite 3 service center project"
+git remote add origin https://github.com/freaM33/vite.maket.git
+git branch -M main
+git push -u origin main
+pause 
